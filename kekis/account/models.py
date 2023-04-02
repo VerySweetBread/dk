62268ce020917de5aa44ccdccbb336771d568cc5
Borsh69 <uludintsevkot@gmail.com>
2023-04-02 20:53:21 +0300

    Add static file

--- kekis/account/models.py
+++ kekis/account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    age = models.IntegerField()
+    email = models.EmailField(max_length=200)
+    teacher = models.CharField(max_length=40)
+    time_created = models.DateTimeField(auto_now_add=True)
