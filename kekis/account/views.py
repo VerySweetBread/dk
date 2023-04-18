be0152d5b03a7e2e06693ec7f4757fa69eff5c1f
Borsh69 <uludintsevkot@gmail.com>
2023-04-18 23:03:05 +0300

    Bag ustranen i ne ugrazhet nihey zhizni

--- kekis/account/views.py
+++ kekis/account/views.py
@@ -4,9 +4,4 @@ from .models import Account
 def main(request,pk):
     account = Account.objects.get(name=pk)
     context = {'account':account}
-<<<<<<< HEAD
     return render(request,'account/account.html', context)
-=======
-    return render(request,'account/account.html', context)
-    
->>>>>>> 3977d66382862d63c2a04e4b105e41d7f87b28e0
