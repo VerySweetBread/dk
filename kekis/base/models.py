919ca92d9ad26cc1dfd2f542f4868a8272815e17
ficuuzzz <dadyagosha@gmail.com>
2023-05-05 19:57:18 +0300

    gosha new style v2

--- kekis/base/models.py
+++ kekis/base/models.py
@@ -19,6 +19,9 @@ class Shop(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     price = models.IntegerField()
 
+    def __str__(self) -> str:
+        return self.title
+
 class Account(models.Model):
     name = models.CharField(max_length=80)
     description = models.CharField(max_length=200)
